8a2d8fc0b8ed75fa1d58565f23d0bc2a4b44c745
Jeremy Hayes <dev@jeremyhayes.net>
2020-11-20 21:42:41 -0500

    loggers page

--- src/Shared/ActuatorService.cs
+++ src/Shared/ActuatorService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
@@ -23,11 +24,26 @@ namespace Hephaestus
         public async Task<JsonElement> GetInfo()
             => await GetResult<JsonElement>("info");
 
+        public async Task<LoggerResponse> GetLoggers()
+            => await GetResult<LoggerResponse>("loggers");
+
         private async Task<T> GetResult<T>(string path)
         {
             var baseUrl = await _js.InvokeAsync<string>("localStorage.getItem", "baseUrl");
             var endpointUrl = $"{baseUrl}/{path}";
             return await _httpClient.GetFromJsonAsync<T>(endpointUrl);
         }
+    }
+
+    public class LoggerResponse
+    {
+        public string[] Levels { get; set; }
+        public IDictionary<string, Logger> Loggers { get; set; }
+    }
+
+    public class Logger
+    {
+        public string EffectiveLevel { get; set; }
+        public string ConfiguredLevel { get; set; }
     }
 }
